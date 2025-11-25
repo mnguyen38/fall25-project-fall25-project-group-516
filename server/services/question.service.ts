84e438979724403ddcc76fe6c273d6c868c93d6c
stevens-ce <stevens.ce@northeastern.edu>
2025-11-24 19:33:53 -0500

    Revert "toggleUserInterest works"
    This reverts commit 698ce005fbb408f2acab746135885bcbd32257a6.

--- server/services/question.service.ts
+++ server/services/question.service.ts
@@ -430,32 +430,39 @@ export const toggleUserInterest = async (
   username: string,
 ): Promise<PopulatedDatabaseQuestion | { error: string }> => {
   try {
-    const currentQ: PopulatedDatabaseQuestion | null = await QuestionModel.findOne({ _id: qid });
-    if (!currentQ) {
-      throw new Error('Question not found');
-    }
-    let q: PopulatedDatabaseQuestion | null;
-    if (currentQ.interestedUsers.includes(username)) {
-      q = await QuestionModel.findOneAndUpdate(
-        { _id: qid },
-        { $pull: { interestedUsers: username } },
-        { new: true },
-      );
-    } else {
-      q = await QuestionModel.findOneAndUpdate(
-        { _id: qid },
-        { $addToSet: { interestedUsers: username } },
-        { new: true },
-      );
-    }
+    const q: PopulatedDatabaseQuestion | null = await QuestionModel.findOneAndUpdate(
+      { _id: new ObjectId(qid) },
+      [
+        {
+          $set: {
+            interestedUsers: {
+              $cond: [
+                { $ne: ['$interestedUsers', undefined] },
+                {
+                  $cond: {
+                    if: { $in: [username, '$interestedUsers'] },
+                    then: {
+                      $setDifference: ['$interestedUsers', [username]],
+                    },
+                    else: { $concatArrays: ['$interestedUsers', [username]] },
+                  },
+                },
+                [username],
+              ],
+            },
+          },
+        },
+      ],
+      { new: true },
+    );
 
     if (!q) {
-      throw new Error('Error updating question');
+      throw new Error('Question not found');
     }
 
     return q;
   } catch (error) {
-    return { error: `Error when fetching and updating interested users: ${error}` };
+    return { error: 'Error when fetching and updating interested users' };
   }
 };
 
